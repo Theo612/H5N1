c542cfd18d7b27a31547966666f144b8e69d362f
Theo612 <156935881+Theo612@users.noreply.github.com>
2024-12-02 15:05:27 +0100

    Delete sequence.py

--- sequence.py
+++
@@ -1,7 +0,0 @@
-class Sequence:
-    def __init__(self, id, sequence):
-        self.id = id
-        self.sequence = sequence
-
-    def __str__(self):
-        return f"Sequence ID: {self.id}, Length: {len(self.sequence)}"
