5930307f49062fdbd400746150584b024f1e088c
Theo612 <156935881+Theo612@users.noreply.github.com>
2024-12-02 10:54:54 +0100

    Update ui.R

--- ui.R
+++ ui.R
@@ -24,39 +24,12 @@ ui <- dashboardPage(
       choices = c("Aucune sélection" = "", list.files("data", pattern = ".fasta")), 
       selected = ""
     ),
-    actionButton("analyze", "Lancer l'analyse")
+    actionButton("analyze", "Lancer l'analyse"),
+    textOutput("debugOutput")
   ),
   
   #Contenu principal
   dashboardBody(
-    #Style personnalisé pour occuper toute la hauteur
-    tags$head(
-      tags$style(HTML("
-        .content-wrapper, .right-side {
-          height: calc(100vh - 50px); /* Prend toute la hauteur de la fenêtre moins l'en-tête */
-          overflow-y: hidden;
-        }
-
-        .tab-content {
-          height: 100%;
-          overflow-y: auto; 
-        }
-
-        iframe {
-          height: calc(100vh - 120px); /* Ajustement pour l'en-tête et l'espacement */
-          width: 100%;
-          border: none; 
-        }
-
-        body {
-          margin: 0;
-          padding: 0;
-          height: 100vh;
-          overflow: hidden;
-        }
-      "))
-    ),
-    
     tabsetPanel(
       tabPanel("Analyse globale", verbatimTextOutput("globalAnalysis")),
       tabPanel("Analyse locale", verbatimTextOutput("localAnalysis")),
