f81dde7c8253118d6c5ba19069050a377d3501ca
Theo612 <156935881+Theo612@users.noreply.github.com>
2024-12-02 16:00:03 +0100

    Update databank.R

--- databank.R
+++ databank.R
@@ -1,30 +1,5 @@
 library(jsonlite)
 
-# Fonction pour lire les fichiers FASTA
-read_fasta <- function(file_path) {
-  lines <- readLines(file_path)
-  sequences <- list()
-  current_id <- NULL
-  current_seq <- ""
-  
-  for (line in lines) {
-    if (startsWith(line, ">")) {
-      if (!is.null(current_id)) {
-        sequences[[current_id]] <- current_seq
-      }
-      current_id <- substr(line, 2, nchar(line))
-      current_seq <- ""
-    } else {
-      current_seq <- paste0(current_seq, line)
-    }
-  }
-  if (!is.null(current_id)) {
-    sequences[[current_id]] <- current_seq
-  }
-  
-  return(sequences)
-}
-
 # Fonction pour convertir un dossier de FASTA en JSON
 convert_folder_fasta_to_json <- function(folder_path, json_file) {
   sequence_db <- list(sequences = list())
