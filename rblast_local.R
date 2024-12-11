68c4a53d2ebb283ee972e2e1df5df04d33c5a388
Rocamadourr <151071894+Rocamadourr@users.noreply.github.com>
2024-12-11 15:39:27 +0100

    Update rblast_local.R

--- rblast_local.R
+++ rblast_local.R
@@ -1,14 +1,15 @@
 run_rblast <-function(fasta_file, bdd_rblastlocal){
 
+# Vérifie si le fasta a comparer est chargé
 if (!file.exists(fasta_file)) {
     stop("Le fichier FASTA spécifié n'existe pas.")
 }
 
-seq <- Biostrings::readDNAStringSet(fasta_file)
+seq <- Biostrings::readDNAStringSet(fasta_file) # Stocke les séquences sans les identifiants
 
-bl <- blast(db = bdd_rblastlocal)
+bl <- blast(db = bdd_rblastlocal) # Charge la base de données
 print(bl)
-cl <- predict(bl, seq, BLAST_args = "-perc_identity 70")
+cl <- predict(bl, seq, BLAST_args = "-perc_identity 70") # Stocke les résultats
 
 return(cl)
 }
